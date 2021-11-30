f82abab6a1a5a5945e62824243fa3da467a53917
ryane <ryanejackson97@yahoo.com>
2021-11-29 20:10:21 -0500

    Added onclick listener for fab, username field for add friend screen, and added starter DB code for the friends DB.

--- app/src/main/java/com/example/smitechooser/AddFriendActivity.kt
+++ app/src/main/java/com/example/smitechooser/AddFriendActivity.kt
@@ -2,11 +2,43 @@ package com.example.smitechooser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import com.example.smitechooser.databinding.ActivityAddFriendBinding
+private lateinit var binding: ActivityAddFriendBinding
 class AddFriendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        //this screen needs a back button? or there is a mem leak, Run window showing "too much work on main thread currently"
+        // need to add a username field, and then learn how to make database to save and query into
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_friend)
+        binding = ActivityAddFriendBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //set up FAB
+        binding.floatingActionButton.setOnClickListener(btnListener)
+    }
+    val btnListener = View.OnClickListener { view ->
+        when (view.getId()) {
+            R.id.floatingActionButton -> {
+                //add name and username to database if isn't already in database
+
+                // below we have created
+                // a new DBHelper class,
+                // and passed context to it
+                val db = DBHelper(this, null)
+
+                // creating variables for values
+                // in name and age edit texts
+                val name = binding.editTextTextPersonName.text.toString()
+                val gamerTag = binding.gamerTag.text.toString()
+
+                // calling method to add
+                // name to our database
+                db.addName(name, gamerTag)
+
+                // at last, clearing edit texts
+                binding.editTextTextPersonName.text.clear()
+                binding.gamerTag.text.clear()
+            }
+        }
     }
 }
