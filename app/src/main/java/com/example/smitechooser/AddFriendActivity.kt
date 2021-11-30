307d3813346e997b75982f053203ecc06605dcc7
ryane <ryanejackson97@yahoo.com>
2021-11-29 20:16:30 -0500

    Added button and textview for test, need to adjust the button code so the TextView gets populated appropriately

--- app/src/main/java/com/example/smitechooser/AddFriendActivity.kt
+++ app/src/main/java/com/example/smitechooser/AddFriendActivity.kt
@@ -39,6 +39,33 @@ class AddFriendActivity : AppCompatActivity() {
                 binding.editTextTextPersonName.text.clear()
                 binding.gamerTag.text.clear()
             }
+            R.id.button ->{
+                // creating a DBHelper class
+                // and passing context to it
+                val db = DBHelper(this, null)
+
+                // below is the variable for cursor
+                // we have called method to get
+                // all names from our database
+                // and add to name text view
+                val cursor = db.getName()
+
+                // moving the cursor to first position and
+                // appending value in the text view
+                cursor!!.moveToFirst()
+                Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n")
+                Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n")
+
+                // moving our cursor to next
+                // position and appending values
+                while(cursor.moveToNext()){
+                    Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n")
+                    Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n")
+                }
+
+                // at last we close our cursor
+                cursor.close()
+            }
         }
     }
 }
